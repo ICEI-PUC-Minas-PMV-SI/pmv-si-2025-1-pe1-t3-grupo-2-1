a404ac15a00f0ef569cc8a477d166eedbece1680
Lucas Rosa <l_r_rosa@hotmail.com>
2025-05-06 17:03:12 -0300

    feat: conteúdo da pagina de detalhes e componentização do header

---
+++ src/js/header.js
@@ -0,0 +1,9 @@
+const header = document.getElementById("app-header")
+
+header.innerHTML = `
+<nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom">
+    <div class="container text-center justify-content-center">
+        <a class="navbar-brand" href="#">Organize. Collect.</a>
+    </div>
+</nav>
+`
