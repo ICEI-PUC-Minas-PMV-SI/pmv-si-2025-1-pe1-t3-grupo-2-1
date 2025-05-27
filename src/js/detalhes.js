5801c6df701834d592690fbc9c5f39f431d00623
Jeff <jefersonalmei03>
2025-05-27 20:07:03 -0300

    adicionando elementos js detalhes.html

--- src/js/detalhes.js
+++ src/js/detalhes.js
@@ -11,5 +11,12 @@ function voltarDetalhes() {
     window.location.href ="detalhes.html";
 }
 function editarValores() {
-    window.location.href ="editar.html";
+    window.location.href ="editar.html";  
+}
+
+function salvarEmprestado() {
+    const inputElement = document.getElementById("empretadoPara");
+  const inputValue = inputElement.value;
+  localStorage.setItem("emprestado_para", inputValue);
+  alert("Texto salvo no localStorage!");
 }
