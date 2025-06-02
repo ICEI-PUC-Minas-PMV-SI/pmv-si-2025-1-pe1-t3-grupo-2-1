9a498d1066ebb1b91aa87cb11533b369dabde53f
Lucas Rosa <l_r_rosa@hotmail.com>
2025-06-02 20:40:34 -0300

    descomentando codigo em detalhes de coleção

--- src/js/editar.js
+++ src/js/editar.js
@@ -4,26 +4,25 @@ function voltarDetalhesItem() {
     window.location.href ="../item/detalhes.html";
 }
 
-// TODO: implementar logica de salvar a edição do item
-// function salvarEdicaoItem() {
-//     const nome = document.getElementById("nome").value;
-//     const descricao = document.getElementById("descricao").value;
-//     const categoria = document.getElementById("categoria").value;
-//     const imagem = document.getElementById("imagem").value;
+function salvarEdicaoItem() {
+    const nome = document.getElementById("nome").value;
+    const descricao = document.getElementById("descricao").value;
+    const categoria = document.getElementById("categoria").value;
+    const imagem = document.getElementById("imagem").value;
 
-//     const item = {
-//         id: localStorage.getItem('activeItemColecao'),
-//         nome,
-//         descricao,
-//         categoria,
-//         imagem
-//     };
-//     editItemColecao(item);
-//     setActiveItemColecao(item.id);
-//     window.location.href ="../item/detalhes.html";
-// }
+    const item = {
+        id: localStorage.getItem('activeItemColecao'),
+        nome,
+        descricao,
+        categoria,
+        imagem
+    };
+    editItemColecao(item);
+    setActiveItemColecao(item.id);
+    window.location.href ="../item/detalhes.html";
+}
 
 
-// const btnSalvar = document.getElementById("savebtn");
+const btnSalvar = document.getElementById("savebtn");
 
-// btnSalvar.addEventListener("click", salvarEdicaoItem);
+btnSalvar.addEventListener("click", salvarEdicaoItem);
