84db1343317468e8b81d11dbbdfde13ac2d959bf
Lucas Rosa <l_r_rosa@hotmail.com>
2025-06-23 22:30:20 -0300

    Função de pesquisa de itens

--- src/js/detalhes-colecao.js
+++ src/js/detalhes-colecao.js
@@ -1,6 +1,7 @@
 import { getActiveCollection, deleteCollection, clearActiveCollection } from "./colecao/colecao.js";
 import { setActiveItemColecao } from "./item/item-colecao.js";
 
+
 function navigateToItemDetails(itemId) {
     setActiveItemColecao(itemId)
     window.location.href = '../item/detalhes.html'
@@ -21,6 +22,7 @@ tabs.forEach(tab => {
 
 
 const activeCollection = getActiveCollection();
+let items = activeCollection.items
 
 if(!activeCollection) {
     window.location.href = '../index.html'
@@ -31,46 +33,47 @@ title.innerText = activeCollection.nome
 const itemsNumber = document.getElementById('items-number-test');
 itemsNumber.innerHTML = `<p>Número de itens: ${activeCollection.items.length}</p>` 
 
+function renderItems() {
 
-const itemsList = document.getElementById('items-list');
-activeCollection.items.forEach(item => {
-    const div = document.createElement('div');
-    
-    div.classList.add('list-group-item')
-    div.classList.add('list-group-item-action')
-    div.classList.add('d-flex')
-    div.classList.add('flex-column')
-    div.classList.add('gap-1')
-    div.classList.add('align-items-start')
-    div.innerHTML = `
-        <div class="fw-bold">${item.nome}</div>
-        <small class="">${item.descricao}</small>
-        <div class="d-flex flex-row gap-2">
-            ${item.categorias ? item.categorias.reduce((acc,categoria) => `${acc}<span class="badge bg-secondary rounded-pill">${categoria}</span>`, '') : ''}
-        </div>
-        <div class="w-100 d-flex flex-row gap-2 justify-content-end">
-            <button type="button" id="detalhes-item-${item.id}" class="btn btn-primary btn-sm">
-                Detalhes
-            </button>
-        </div>
-    `;
-    itemsList.appendChild(div);
-});
-
-activeCollection.items.forEach(item => {
-    const detalhesButton = document.getElementById(`detalhes-item-${item.id}`);
-    detalhesButton.addEventListener('click', function() {
-        navigateToItemDetails(item.id);
+    const itemsList = document.getElementById('items-list');
+    itemsList.innerHTML = ''
+    items.forEach(item => {
+        const div = document.createElement('div');
+        
+        div.classList.add('list-group-item')
+        div.classList.add('list-group-item-action')
+        div.classList.add('d-flex')
+        div.classList.add('flex-column')
+        div.classList.add('gap-1')
+        div.classList.add('align-items-start')
+        div.innerHTML = `
+            <div class="fw-bold">${item.nome}</div>
+            <small class="">${item.descricao}</small>
+            <div class="d-flex flex-row gap-2">
+                ${item.categorias ? item.categorias.reduce((acc,categoria) => `${acc}<span class="badge bg-secondary rounded-pill">${categoria}</span>`, '') : ''}
+            </div>
+            <div class="w-100 d-flex flex-row gap-2 justify-content-end">
+                <button type="button" id="detalhes-item-${item.id}" class="btn btn-primary btn-sm">
+                    Detalhes
+                </button>
+            </div>
+        `;
+        itemsList.appendChild(div);
     });
-})
+    
+    items.forEach(item => {
+        const detalhesButton = document.getElementById(`detalhes-item-${item.id}`);
+        detalhesButton.addEventListener('click', function() {
+            navigateToItemDetails(item.id);
+        });
+    })
+}
 
-const deleteButton = document.getElementById('exclude-button');
+renderItems()
 
-deleteButton.addEventListener('click', function() {
-    const confirmDelete = confirm('Tem certeza que deseja excluir a coleção?');
-    if(confirmDelete) {
-        deleteCollection(activeCollection.id);
-        clearActiveCollection();
-        window.location.href = '../index.html';
-    }
+const searchInput = document.getElementById('search')
+
+searchInput.addEventListener('change', (e) => {
+    items = activeCollection.items.filter(i => i.nome.toLowerCase().includes(e.target.value.toLowerCase()))
+    renderItems()
 })
